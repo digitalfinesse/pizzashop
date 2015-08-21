bdb575ac17f73387672858e3a5b53d3ea373c04c
Evgeniy Palguev <dizverst@gmail.com>
2015-08-21 18:17:00 +0300

    создал модель

--- app.rb
+++ app.rb
@@ -1,8 +1,10 @@
 require 'rubygems'
+require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 require 'sqlite3'
+
 
 set :database, "sqlite3:pizzashop.db"
 
