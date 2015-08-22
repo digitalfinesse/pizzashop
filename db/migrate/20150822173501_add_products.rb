109b08e5faed1fe1d7c73a7ec1cf6378401ec79b
Evgeniy Palguev <dizverst@gmail.com>
2015-08-22 20:55:37 +0300

    Новые продукты
    добавляем новую миграцию и заполнен новые продукты

---
+++ db/migrate/20150822173501_add_products.rb
@@ -0,0 +1,10 @@
+class AddProducts < ActiveRecord::Migration
+  def change
+  		Product.create :title => 'Гавайская', :description => 'Это гавайская пицца с далеких островов', :price => 350, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => false, :path_to_image => '/images/hawaiian.jpg'
+
+  		Product.create :title => 'Пепперони', :description => 'Пицца пепперони со вкусной колбаской', :price => 450, :size => 30, :is_spicy => false, :is_veg => false, :is_best_offer => true, :path_to_image => '/images/pepperoni.jpg'
+
+  		Product.create :title => 'Гавайская', :description => 'Это гавайская пицца с далеких островов', :price => 350, :size => 30, :is_spicy => false, :is_veg => true, :is_best_offer => false, :path_to_image => '/images/veg.jpg'
+
+  end
+end
